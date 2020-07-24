f335de54bb654d739ebb5102b8bc31f35d9f1ef5
Akash <akash.ns@mahaswami.com>
2020-07-24 13:38:00 +0530

    commented this file to avoid restoring default roles, users, dashboards, charts and default settings.

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -15,6 +15,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Remove below line and last line to uncomment this file.
+<<'COMMENTS'
 set -e
 
 STEP_CNT=4
@@ -59,3 +62,5 @@ fi
 echo_step "4" "Starting" "Setting up roles and perms"
 superset init
 echo_step "4" "Complete" "Setting up roles and perms"
+
+COMMENTS
